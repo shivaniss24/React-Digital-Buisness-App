abf5bb493174fb1d988ea98e568e44ab3522d8ac
Shivani Soni <eng.shivanisoni2405@gmail.com>
2024-07-14 18:05:07 +0530

    update layout.module.css

--- digitalbuisness/src/App.js
+++ digitalbuisness/src/App.js
@@ -1,12 +1,26 @@
 
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import Dashboard from './pages/Dashboard/Dashboard';
+
+
+
+
 
 function App() {
   return (
-    
- <div id="dashboard">
-Dashboard content
- </div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+          <Route path="Dashboard" element={<Dashboard/>}>
+           </Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+
   );
 }
 
---
+++ digitalbuisness/src/Components/Layout/Layout.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import css from './Layout.module.css';
+import moment from 'moment/moment';
+import {BiSearch} from 'react-icons/bi';
+
+const Layout = () => {
+  return (
+    <div className={css.container}>
+    <div>Sidebar</div>
+    <div className={css.dashboard}>
+        <div className={css.topBaseGradient}>
+            <div className="gradient-red"></div>
+            <div className="gradient-orange"></div>
+            <div className="gradient-blue"></div>
+        </div>
+        <div className={css.header}>
+            <span>{moment().format("dddd, do MMM YYYY")}</span>
+            <div className={css.searchBar}>
+                <BiSearch size={20}/>
+                <input type="text" placeholder='Enter here'/>
+            </div>
+            <div className={css.profile}>
+                <img src="./profile.jpg" alt=""  />
+              <div className={css.details}>
+                <span>Shivani Soni</span>
+                <span> eng.shivanisoni2405@gmail.com</span>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Layout
