5e41797f9f83d046d607a05ecfd3fd578e86c8a7
Shivani Soni <eng.shivanisoni2405@gmail.com>
2024-07-15 16:21:16 +0530

    adding statisticalcahrt

--- digitalbuisness/src/Components/Statistics/Statistics.js
+++ digitalbuisness/src/Components/Statistics/Statistics.js
@@ -2,6 +2,7 @@ import React from 'react'
 import css from './Statistics.module.css';
 import {BsArrowUpShort} from 'react-icons/bs';
 import { groupNumber } from '../../data/data';
+import StatisticalChart from '../../Components/StatisticalChart/StatisticalChart'
 const Statistics = () => {
   return (
     <div className={`${css.container} theme-container`}>
@@ -39,6 +40,7 @@ const Statistics = () => {
     <span>$ {groupNumber(57348)}</span>
 </div>
     </div>
+    <StatisticalChart/>
     </div>
     )
 }
