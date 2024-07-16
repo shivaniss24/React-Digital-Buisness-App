bbed81c5b2feb50314bfca9c2cebef0cd85e94f4
Shivani Soni <eng.shivanisoni2405@gmail.com>
2024-07-16 17:52:24 +0530

    add event listeners inside calendarjs

--- digitalbuisness/src/pages/Calendar/Calendar.js
+++ digitalbuisness/src/pages/Calendar/Calendar.js
@@ -1,36 +1,67 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import useCalender from '../../store/Calendar';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayDridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import "./Calendar.css";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import useCalender from "../../store/Calendar";
+import { createEventId } from "../../data/data";
+/* eslint-disable no-restricted-globals */
 
 const Calendar = () => {
-const {currentEvents}=useCalender;
+  const { currentEvents, setCurrentEvents } = useCalender();
 
+  const handleEvents = async (events) => {
+    await Promise.resolve(setCurrentEvents(events));
+  };
+
+  const handleDataSelect = (selectInfo) => {
+    let title = prompt("Please enter the title for the event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect();
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.start,
+        end: selectInfo.end,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+  const handleEventClick = (clickInfo) => {
+    if (confirm("Are you sure you want to delete it ?")) {
+      clickInfo.event.remove();
+    }
+  };
   return (
     <div className="calendar-container">
-  <FullCalendar   plugins={[dayGridPlugin,interactionPlugin,timeGridPlugin]} 
-  headerToolbar={{
-    left:'orev,next today',
-    center:"title",
-    right:"dayGridMonth,timeGridMonth,timeGridDay"
-  }}
-  allDaySlot={false}
-  initialView="timeGridWeek"
-  slotDuration={"01:00:00"}
-  editable={true}
-  selectable={true}
-  selectMirror={true}
-  dayMaxEvents={true}
-  weekends={true}
-  nowIndicator={true}
-  initialEvents={[currentEvents]}
-  />
-</div>
-
-  )
-
-}
+      <div>
+        <FullCalendar
+          plugins={[dayDridPlugin, interactionPlugin, timeGridPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          allDaySlot={false}
+          initialView="timeGridWeek"
+          slotDuration={"01:00:00"}
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          datMaxEvents={true}
+          weekends={true}
+          nowIndicator={true}
+          initialEvents={currentEvents}
+          eventsSet={handleEvents}
+          select={handleDataSelect}
+          eventClick={handleEventClick}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Calendar;
--- digitalbuisness/src/store/Calendar.js
+++ digitalbuisness/src/store/Calendar.js
@@ -1,5 +1,5 @@
 import {create} from 'zustand';
-import { cardsData,ordersData,groupNumber,INITIAL_EVENTS,boardData,userData } from '../../data/data';
+import { cardsData,ordersData,groupNumber,INITIAL_EVENTS,boardData,userData } from '../data/data';
 const useCalender = create((set) => ({
     currentEvents: INITIAL_EVENTS,
     setCurrentEvents: (events) => set({ currentEvents: events }),
